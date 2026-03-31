0e0828cd787ae1aa41698db3df9a9e4196aeccbd
banabir barua <banabir4870@gmail.com>
2026-03-31 20:09:31 +0600

    added stats section and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Banner from './Components/Banner/Banner'
 import Navbar from './Components/Navbar/Navbar'
+import Stats from './Components/Stats/Stats'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar/>
     <Banner/>
+    <Stats/>
     </>
   )
 }
