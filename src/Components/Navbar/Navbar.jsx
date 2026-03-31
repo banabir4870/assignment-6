6f97f553edcab1bbd3a989c082ce168d612ca0b5
banabir barua <banabir4870@gmail.com>
2026-03-31 08:31:24 +0600

    added navbar and navbar responsiveness

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
+
+const Navbar = () => {
+    return (
+        <div className='bg-base-100 shadow-sm'>
+            <div className="navbar md:w-10/12 mx-auto">
+                <div className="navbar-start">
+                    <a className="font-extrabold text-3xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">daisyUI</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal gap-8 px-1 text-lg font-semibold">
+                        <li>
+                            <a>Products</a>
+                        </li>
+                        <li>
+                            <a>Features</a>
+                        </li>
+                        <li>
+                            <a>Pricing</a>
+                        </li>
+                        <li>
+                            <a>Testimonials</a>
+                        </li>
+                        <li>
+                            <a>FAQ</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="navbar-end gap-3">
+                    <FiShoppingCart />
+                    <a className='font-semibold'>Login</a>
+                    <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white">Get Started</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
