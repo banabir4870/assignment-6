12aa50054ca863f8eab79f3e775519b50764f91d
banabir barua <banabir4870@gmail.com>
2026-04-01 20:57:18 +0600

    checked all files and made changes where need

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -3,7 +3,6 @@ import Tool from './Tool';
 
 const Products = ({productsPromise, cart, setCart}) => {
     const products = use(productsPromise)
-    console.log(products)
     return (
         <div className='w-8/12 mx-auto grid md:grid-cols-2 lg:grid-cols-3 mt-10 mb-32 gap-8'>
             {
